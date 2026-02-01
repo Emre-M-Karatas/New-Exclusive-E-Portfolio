c00594c3adffb1f0f4151cf519ccc5bbd18b072d
Emre-M-Karatas <emre.karatas1@hotmail.com>
2026-02-01 16:01:58 +1100

    Email javascript finished

---
+++ index.js
@@ -0,0 +1,24 @@
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_zypanze",
+      "template_wckhxq5",
+      event.target,
+      "FXjjbKigPnLF0bT0D",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on emre.karatas1@hotmail.com",
+      );
+    });
+}
